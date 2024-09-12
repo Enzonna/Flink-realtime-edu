9619b5f77236ac2ea0fab0e27349f084e301f9b9
enzo <enzo@enzo.com>
2024-09-12 16:25:29 +0800

    2024-09-12 DIM层完成 2024-09-12 DwdBaseLog层完成

---
+++ realtime-common/src/main/java/com/atguigu/edu/realtime/common/util/FlinkSinkUtil.java
@@ -0,0 +1,86 @@
+package com.atguigu.edu.realtime.common.util;
+
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.edu.realtime.common.bean.TableProcessDwd;
+import com.atguigu.edu.realtime.common.constant.Constant;
+import org.apache.doris.flink.cfg.DorisExecutionOptions;
+import org.apache.doris.flink.cfg.DorisOptions;
+import org.apache.doris.flink.cfg.DorisReadOptions;
+import org.apache.doris.flink.sink.DorisSink;
+import org.apache.doris.flink.sink.writer.serializer.SimpleStringSerializer;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import javax.annotation.Nullable;
+import java.util.Properties;
+
+public class FlinkSinkUtil {
+    // get kafkaSink
+    public static KafkaSink<String> getKafkaSink(String topic) {
+        KafkaSink<String> kafkaSink = KafkaSink.<String>builder()
+                .setBootstrapServers(Constant.KAFKA_BROKERS)
+                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setTopic(topic)
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build())
+                //.setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                //.setTransactionalIdPrefix("xxx")
+                //.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 15 * 60 * 1000 + "")
+                .build();
+        return kafkaSink;
+    }
+
+
+    public static KafkaSink<Tuple2<JSONObject, TableProcessDwd>> getKafkaSink() {
+        KafkaSink<Tuple2<JSONObject, TableProcessDwd>> kafkaSink = KafkaSink.<Tuple2<JSONObject, TableProcessDwd>>builder()
+                .setBootstrapServers(Constant.KAFKA_BROKERS)
+                .setRecordSerializer(
+                        new KafkaRecordSerializationSchema<Tuple2<JSONObject, TableProcessDwd>>() {
+                            @Nullable
+                            @Override
+                            public ProducerRecord<byte[], byte[]> serialize(Tuple2<JSONObject, TableProcessDwd> tup2, KafkaSinkContext context, Long timestamp) {
+                                JSONObject jsonObj = tup2.f0;
+                                TableProcessDwd tableProcessDwd = tup2.f1;
+                                String topic = tableProcessDwd.getSinkTable();
+                                return new ProducerRecord<byte[], byte[]>(topic, jsonObj.toJSONString().getBytes());
+                            }
+                        }
+                )
+                //.setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                //.setTransactionalIdPrefix("xxx")
+                //.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG,15*60*1000 +"")
+                .build();
+        return kafkaSink;
+    }
+
+
+    // 获取DorisSink
+    public static DorisSink<String> getDorisSink(String tableName) {
+
+        Properties props = new Properties();
+        props.setProperty("format", "json");
+        props.setProperty("read_json_by_line", "true"); // 每行一条 json 数据
+
+        DorisSink<String> sink = DorisSink.<String>builder()
+                .setDorisReadOptions(DorisReadOptions.builder().build())
+                .setDorisOptions(DorisOptions.builder() // 设置 doris 的连接参数
+                        .setFenodes(Constant.DORIS_FE_NODES)
+                        .setTableIdentifier(Constant.DORIS_DATABASE + "." + tableName)
+                        .setUsername("root")
+                        .setPassword("aaaaaa")
+                        .build())
+                .setDorisExecutionOptions(DorisExecutionOptions.builder() // 执行参数
+                        //.setLabelPrefix("doris-label")  // stream-load 导入的时候的 label 前缀
+                        .disable2PC() // 开启两阶段提交后,labelPrefix 需要全局唯一,为了测试方便禁用两阶段提交
+                        .setDeletable(false)
+                        .setMaxRetries(3)
+                        .setStreamLoadProp(props)
+                        .build())
+                .setSerializer(new SimpleStringSerializer())
+                .build();
+        return sink;
+    }
+}
--- realtime-dwd/realtime-dwd-base-log/src/main/java/com/atguigu/edu/realtime/dwd/log/split/app/DwdBaseLog.java
+++ realtime-dwd/realtime-dwd-base-log/src/main/java/com/atguigu/edu/realtime/dwd/log/split/app/DwdBaseLog.java
@@ -1,17 +1,228 @@
 package com.atguigu.edu.realtime.dwd.log.split.app;
 
-import com.enzo.gmall.realtime.common.base.BaseApp;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.edu.realtime.common.base.BaseApp;
+import com.atguigu.edu.realtime.common.constant.Constant;
+import com.atguigu.edu.realtime.common.util.DateFormatUtil;
+import com.atguigu.edu.realtime.common.util.FlinkSinkUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.streaming.api.datastream.*;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DwdBaseLog extends BaseApp {
+    private final String ERR = "err";
+    private final String START = "start";
+    private final String DISPLAY = "display";
+    private final String ACTION = "action";
+    private final String PAGE = "page";
+
     public static void main(String[] args) {
-        new DwdBaseLog().start(10011, 4, "dwd_base_log", "topic_log");
+        new DwdBaseLog().start(10012, 4, "dwd_base_log", "topic_log");
     }
 
 
     @Override
-    public void handle(StreamExecutionEnvironment streamExecutionEnvironment, DataStreamSource<String> dataStreamSource) {
+    public void handle(StreamExecutionEnvironment env, DataStreamSource<String> kafkaStrDS) {
+        // TODO 1. Perform type conversion on streaming data and perform simple ETL     jsonStr -> jsonObj
+        SingleOutputStreamOperator<JSONObject> jsonObjDS = etl(kafkaStrDS);
+        // jsonObjDS.print("etl✅✅:");
 
+        // TODO 2. Repairs 新老访客 label
+        SingleOutputStreamOperator<JSONObject> fixedDS = fixedNewAndOld(jsonObjDS);
+        // fixedDS.print("fixed:");
+
+        // TODO 3. Splitting, putting different types of logs into different streams
+        Map<String, DataStream> streamMap = splitStream(fixedDS);
+
+        // TODO 4. Write different data to the topic of Kafka
+        writeToKafka(streamMap);
+    }
+
+
+    private void writeToKafka(Map<String, DataStream> streamMap) {
+        streamMap
+                .get(PAGE)
+                .sinkTo(FlinkSinkUtil.getKafkaSink(Constant.TOPIC_DWD_TRAFFIC_PAGE));
+        streamMap
+                .get(ERR)
+                .sinkTo(FlinkSinkUtil.getKafkaSink(Constant.TOPIC_DWD_TRAFFIC_ERR));
+        streamMap
+                .get(START)
+                .sinkTo(FlinkSinkUtil.getKafkaSink(Constant.TOPIC_DWD_TRAFFIC_START));
+        streamMap
+                .get(DISPLAY)
+                .sinkTo(FlinkSinkUtil.getKafkaSink(Constant.TOPIC_DWD_TRAFFIC_DISPLAY));
+        streamMap
+                .get(ACTION)
+                .sinkTo(FlinkSinkUtil.getKafkaSink(Constant.TOPIC_DWD_TRAFFIC_ACTION));
+    }
+
+    private Map<String, DataStream> splitStream(SingleOutputStreamOperator<JSONObject> fixedDS) {
+        OutputTag<String> errTag = new OutputTag<String>("errTag") {
+        };
+        OutputTag<String> startTag = new OutputTag<String>("startTag") {
+        };
+        OutputTag<String> displayTag = new OutputTag<String>("displayTag") {
+        };
+        OutputTag<String> actionTag = new OutputTag<String>("actionTag") {
+        };
+        SingleOutputStreamOperator<String> pageDS = fixedDS.process(
+                new ProcessFunction<JSONObject, String>() {
+                    @Override
+                    public void processElement(JSONObject jsonObj, ProcessFunction<JSONObject, String>.Context ctx, Collector<String> out) throws Exception {
+                        JSONObject errJsonObj = jsonObj.getJSONObject("err");
+                        if (errJsonObj != null) {
+                            ctx.output(errTag, jsonObj.toJSONString());
+                            jsonObj.remove("err");
+                        }
+
+                        JSONObject startJsonObj = jsonObj.getJSONObject("start");
+                        if (startJsonObj != null) {
+                            ctx.output(startTag, jsonObj.toJSONString());
+                        } else {
+                            JSONObject commonJsonObj = jsonObj.getJSONObject("common");
+                            JSONObject pageJsonObj = jsonObj.getJSONObject("page");
+                            Long ts = jsonObj.getLong("ts");
+
+                            JSONArray displaysArr = jsonObj.getJSONArray("displays");
+                            if (displaysArr != null && displaysArr.size() > 0) {
+                                for (int i = 0; i < displaysArr.size(); i++) {
+                                    JSONObject displayJsonObj = displaysArr.getJSONObject(i);
+                                    JSONObject newDisplayJsonObj = new JSONObject();
+                                    newDisplayJsonObj.put("common", commonJsonObj);
+                                    newDisplayJsonObj.put("page", pageJsonObj);
+                                    newDisplayJsonObj.put("ts", ts);
+                                    newDisplayJsonObj.put("display", displayJsonObj);
+                                    ctx.output(displayTag, newDisplayJsonObj.toJSONString());
+                                }
+                                jsonObj.remove("displays");
+                            }
+
+
+                            JSONArray actionsArr = jsonObj.getJSONArray("actions");
+                            if (actionsArr != null && actionsArr.size() > 0) {
+                                for (int i = 0; i < actionsArr.size(); i++) {
+                                    JSONObject actionJsonObj = actionsArr.getJSONObject(i);
+                                    JSONObject newActionJsonObj = new JSONObject();
+                                    newActionJsonObj.put("common", commonJsonObj);
+                                    newActionJsonObj.put("page", pageJsonObj);
+                                    newActionJsonObj.put("action", actionJsonObj);
+                                    ctx.output(actionTag, newActionJsonObj.toJSONString());
+                                }
+                                jsonObj.remove("actions");
+                            }
+
+
+                            out.collect(jsonObj.toJSONString());
+
+                        }
+                    }
+                }
+        );
+        SideOutputDataStream<String> errDS = pageDS.getSideOutput(errTag);
+        SideOutputDataStream<String> startDS = pageDS.getSideOutput(startTag);
+        SideOutputDataStream<String> displayDS = pageDS.getSideOutput(displayTag);
+        SideOutputDataStream<String> actionDS = pageDS.getSideOutput(actionTag);
+
+
+        errDS.print("err--:✅");
+        pageDS.print("page--:✅");
+        startDS.print("start--:✅");
+        displayDS.print("display--:✅");
+        actionDS.print("action--:✅");
+
+        Map<String, DataStream> splitMap = new HashMap<>();
+        splitMap.put("err", errDS);
+        splitMap.put("start", startDS);
+        splitMap.put("display", displayDS);
+        splitMap.put("action", actionDS);
+        splitMap.put("page", pageDS);
+
+        return splitMap;
+    }
+
+    private static SingleOutputStreamOperator<JSONObject> fixedNewAndOld(SingleOutputStreamOperator<JSONObject> jsonObjDS) {
+        KeyedStream<JSONObject, String> keyedDS =
+                jsonObjDS.keyBy(jsonObj -> jsonObj.getJSONObject("common").getString("mid"));
+
+        SingleOutputStreamOperator<JSONObject> fixedDS = keyedDS.process(
+                new KeyedProcessFunction<String, JSONObject, JSONObject>() {
+
+                    private ValueState<String> lastVisitDateState;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        ValueStateDescriptor<String> valueStateDescriptor =
+                                new ValueStateDescriptor<>("lastVisitDate", String.class);
+                        lastVisitDateState = getRuntimeContext().getState(valueStateDescriptor);
+                    }
+
+                    @Override
+                    public void processElement(JSONObject jsonObj, KeyedProcessFunction<String, JSONObject, JSONObject>.Context ctx, Collector<JSONObject> out) throws Exception {
+                        String isNew = jsonObj.getJSONObject("common").getString("is_new");
+                        String lastVisitDate = lastVisitDateState.value();
+                        Long ts = jsonObj.getLong("ts");
+                        String curVisitDate = DateFormatUtil.tsToDate(ts);
+
+                        if ("1".equals(isNew)) {
+                            if (StringUtils.isEmpty(lastVisitDate)) {
+                                lastVisitDateState.update(curVisitDate);
+                            } else {
+                                if (!lastVisitDate.equals(curVisitDate)) {
+                                    isNew = "0";
+                                    jsonObj.getJSONObject("common").put("is_new", isNew);
+                                }
+                            }
+
+                        } else {
+                            if (StringUtils.isEmpty(lastVisitDate)) {
+                                long yesterDayTs = ts - 24 * 60 * 60 * 1000;
+                                String yesterDay = DateFormatUtil.tsToDate(yesterDayTs);
+                                lastVisitDateState.update(yesterDay);
+                            }
+                        }
+                        out.collect(jsonObj);
+                    }
+                }
+        );
+        return fixedDS;
+    }
+
+    private static SingleOutputStreamOperator<JSONObject> etl(DataStreamSource<String> kafkaStrDS) {
+        OutputTag<String> dirtyTag = new OutputTag<String>("dirtyTag") {
+        };
+
+        SingleOutputStreamOperator<JSONObject> jsonObjDS = kafkaStrDS.process(
+                new ProcessFunction<String, JSONObject>() {
+                    @Override
+                    public void processElement(String jsonStr, ProcessFunction<String, JSONObject>.Context ctx, Collector<JSONObject> out) throws Exception {
+                        try {
+                            JSONObject jsonObj = JSON.parseObject(jsonStr);
+                            out.collect(jsonObj);
+                        } catch (Exception e) {
+                            ctx.output(dirtyTag, jsonStr);
+                        }
+                    }
+                }
+        );
+
+        SideOutputDataStream<String> dirtyDS = jsonObjDS.getSideOutput(dirtyTag);
+        KafkaSink<String> kafkaSink = FlinkSinkUtil.getKafkaSink("dirty_data");
+        dirtyDS.sinkTo(kafkaSink);
+        return jsonObjDS;
     }
 }
